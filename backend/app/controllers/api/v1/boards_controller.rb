e072dce9ebaa9e5bf9423611213bc35c3afa774d
Soseh <sosehv@gmail.com>
2025-07-18 13:01:37 -0400

    Adds actions in BoardsController

--- backend/app/controllers/api/v1/boards_controller.rb
+++ backend/app/controllers/api/v1/boards_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::BoardsController < ApplicationController
+  def index
+    boards = Board.all
+    render json: boards
+  end
+
+  def show
+    board = Board.find(params[:id])
+    render json: board, include: :lists
+  end
+
+  def create
+    board = Board.new(board_params)
+    render json: board, status: :created
+    else
+      render json: { errors: board.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    board = Board.find(params[:id])
+    if board.update(board_params)
+      render json: board
+    else
+      render json: { errors: board.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.destroy
+    head :no_content
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
